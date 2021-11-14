24b4f7cc794fcd986dd93f97062003ea02772b09
Malte Franken <exxamalte@users.noreply.github.com>
2021-11-14 20:29:41 +1100

    improves resilience when reading from sensor; fixes #1

--- mlx90614/mlx90614_sensor.h
+++ mlx90614/mlx90614_sensor.h
@@ -3,6 +3,7 @@
 #include "Adafruit_MLX90614.h"
 
 #define TAG "mlx90614"
+#define SENSOR_READ_RETRIES 5
 
 class MLX90614Sensor : public PollingComponent {
 
@@ -16,7 +17,11 @@ class MLX90614Sensor : public PollingComponent {
 
     void setup() override {
       ESP_LOGCONFIG(TAG, "Setting up MLX90614Sensor...");
-      mlx.begin();
+      Serial.begin(9600);
+      if (!mlx.begin()) {
+        ESP_LOGE(TAG, "Error connecting to MLX sensor. Check wiring.");
+        while (1);
+      };
       LOG_SENSOR("  ", "Ambient temperature", this->ambient_temperature_sensor);
       LOG_SENSOR("  ", "Object temperature", this->object_temperature_sensor);
     }
@@ -24,7 +29,13 @@ class MLX90614Sensor : public PollingComponent {
     void update() override {
       // Ambient temperature range: -40°C ... +125˚C
       double ambient_temperature = mlx.readAmbientTempC();
-      if ((-40.0 <= ambient_temperature) && (ambient_temperature <=125.0)) {
+      int counter = 1;
+      while (((ambient_temperature < -40.0) || (ambient_temperature > 125.0)) && counter <= SENSOR_READ_RETRIES) {
+        // Try 5 times.
+        ambient_temperature = mlx.readAmbientTempC();
+        counter++;
+      }
+      if ((-40.0 <= ambient_temperature) && (ambient_temperature <= 125.0)) {
         ESP_LOGD(TAG, "Ambient temperature measured: %f", ambient_temperature);
         ambient_temperature_sensor->publish_state(ambient_temperature);
       } else {
@@ -33,7 +44,13 @@ class MLX90614Sensor : public PollingComponent {
 
       // Object temperature range: -70°C ... +380˚C
       double object_temperature = mlx.readObjectTempC();
-      if ((-70.0 <= object_temperature) && (object_temperature <=380.0)) {
+      counter = 1;
+      while (((object_temperature < -70.0) || (object_temperature > 380.0)) && counter <= SENSOR_READ_RETRIES) {
+        // Try 5 times.
+        object_temperature = mlx.readObjectTempC();
+        counter++;
+      }
+      if ((-70.0 <= object_temperature) && (object_temperature <= 380.0)) {
         ESP_LOGD(TAG, "Object temperature measured: %f", object_temperature);
         object_temperature_sensor->publish_state(object_temperature);
       } else {
