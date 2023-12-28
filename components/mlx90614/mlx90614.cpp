deb27813772b8b5a5849f28b8158e45c204620d9
Malte Franken <exxamalte@users.noreply.github.com>
2023-12-29 00:28:50 +1100

    fixed variable names

--- components/mlx90614/mlx90614.cpp
+++ components/mlx90614/mlx90614.cpp
@@ -18,8 +18,8 @@ void MLX90614Component::setup() {
     ESP_LOGE(TAG, "Error connecting to MLX sensor. Check wiring.");
     while (1);
   };
-  LOG_SENSOR("  ", "Ambient temperature", this->ambient_temperature_sensor);
-  LOG_SENSOR("  ", "Object temperature", this->object_temperature_sensor);
+  LOG_SENSOR("  ", "Ambient temperature", this->ambient_temperature_sensor_);
+  LOG_SENSOR("  ", "Object temperature", this->object_temperature_sensor_);
 }
 
 void MLX90614Component::update() {
@@ -33,7 +33,7 @@ void MLX90614Component::update() {
   }
   if ((-40.0 <= ambient_temperature) && (ambient_temperature <= 125.0)) {
     ESP_LOGD(TAG, "Ambient temperature measured: %f", ambient_temperature);
-    ambient_temperature_sensor->publish_state(ambient_temperature);
+    ambient_temperature_sensor_->publish_state(ambient_temperature);
   } else {
     ESP_LOGW(TAG, "Ambient temperature out of range: %f", ambient_temperature);
   }
@@ -48,7 +48,7 @@ void MLX90614Component::update() {
   }
   if ((-70.0 <= object_temperature) && (object_temperature <= 380.0)) {
     ESP_LOGD(TAG, "Object temperature measured: %f", object_temperature);
-    object_temperature_sensor->publish_state(object_temperature);
+    object_temperature_sensor_->publish_state(object_temperature);
   } else {
     ESP_LOGW(TAG, "Object temperature out of range: %f", object_temperature);
   }
